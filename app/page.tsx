c6642417e46834a636482f2d7acde5df3bcd79ad
thenoobchad <noobchaddy@gmail.com>
2026-01-08 14:11:57 +0100

    fix:product cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,25 @@
 
 import { Button } from "@/components/ui/button";
 import {
+	Bookmark,
 	ChevronDown,
 	Clipboard,
+	Clock,
+	Eye,
 	LayoutGrid,
 	Locate,
 	LocateFixed,
 	LocateIcon,
 	Menu,
 	Search,
+	ShoppingCart,
 	Sparkles,
+	Star,
 	Store,
 	User,
 } from "lucide-react";
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from "@/components/ui/carousel";
-import {
-	Card,
-	CardAction,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+
+
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
@@ -60,7 +51,7 @@ export default function Home() {
 					</div>
 					<div className="flex-1 w-full">
 						<div className="text-sm flex items-center justify-between w-full  p-2 bg-zinc-100 rounded-sm">
-							<div className="flex items-center gap-2 overflow-hidden h-5">
+							<div className="flex items-center gap-6 overflow-hidden h-5">
 								<Search size={20} />
 								<Swiper
 									direction={"vertical"}
@@ -133,6 +124,7 @@ export default function Home() {
 					</div>
 				</div>
 			</header>
+
 			<section className=" px-4 flex flex-col gap-2 w-full">
 				<div className="h-full w-full">
 					<Swiper
@@ -170,7 +162,12 @@ export default function Home() {
 				<div className="h-full w-full">
 					<Swiper
 						spaceBetween={4}
-						modules={[Pagination]}
+						loop={true}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						modules={[Autoplay]}
 						className="mySwiper"
 						breakpoints={{
 							320: {
@@ -201,7 +198,7 @@ export default function Home() {
 				</div>
 			</section>
 
-			<section className=" px-4 flex flex-col gap-2 w-full h-full py-9">
+			<section className=" px-4 flex flex-col gap-2 w-full h-full ">
 				<div className="py-4 flex justify-between items-center">
 					<div className="flex items-center gap-2">
 						<div className="p-2 bg-blue-500 rounded-full">
@@ -246,22 +243,107 @@ export default function Home() {
 											<h1 className="my-1 font-semibold text-sm">
 												TechSphere Electronics
 											</h1>
-											
-												<p className="text-xs flex whitespace-nowrap gap-2 text-zinc-500 pb-4">
-													<span>
-														<LocateFixed size={18} />
-													</span>
-												<span>Kilimanjaro iwofe</span>
-												<span className="bg-green-200 mx-4 px-2 rounded-md">Open</span>
-												</p>
-											
+
+											<p className="text-xs flex whitespace-nowrap gap-2 text-zinc-500 pb-1 justify-between">
+												<span className="flex gap-2">
+													<LocateFixed size={18} />
+												
+												Kilimanjaro iwofe</span>
+												<span className="bg-green-200 mx-4 px-1 rounded-md">
+													0 m
+												</span>
+											</p>
 										</div>
 									</div>
 								</div>
 							</SwiperSlide>
 						))}
 					</Swiper>
-					<div>hey there</div>
+				</div>
+			</section>
+
+			{/* Products */}
+			<section className=" px-4 flex flex-col gap-2 w-full h-full ">
+				<div className="py-2 flex justify-between items-center">
+					<div className="flex items-center gap-2">
+						<div className="p-2 bg-blue-500 rounded-full">
+							<Store className="text-white" size={20} />
+						</div>
+						<div>
+							<h3 className="font-semibold">Wholesome Beginnings</h3>
+							<p className="text-xs text-zinc-500">
+								Healthy, tasty and ready in minutes.
+							</p>
+						</div>
+					</div>
+					<span className="text-">See All</span>
+				</div>
+				<div className="h-full">
+					<Swiper
+						slidesPerView={2}
+						spaceBetween={10}
+						modules={[Pagination]}
+						className="mySwiper"
+						breakpoints={{
+							320: {
+								slidesPerView: 2,
+								spaceBetween: 4,
+							},
+							640: {
+								slidesPerView: 3,
+								spaceBetween: 4,
+							},
+							768: {
+								slidesPerView: 4,
+								spaceBetween: 6,
+							},
+						}}>
+						{Array.from({ length: 9 }).map((_, index) => (
+							<SwiperSlide key={index}>
+								<div className="w-full rounded-md overflow-hidden border border-zinc-300 mb-4 relative">
+									<span className="absolute bg-green-500 text-white px-2 rounded-br-sm text-sm ">
+										7% off
+									</span>
+									<span className="absolute right-2 flex flex-col gap-2 top-2 text-white">
+										<Bookmark size={20} />
+										<Eye size={20} />
+									</span>
+									<img src="/images/amori.jpg" alt="slige-image" />
+									<div className="relative">
+										<div className="flex flex-col p-2 mt-4">
+											<div className="flex items-center gap-1 p-1 text-blue-600 bg-blue-200 rounded-md w-fit">
+												<Clock size={16} />
+												<p className="text-xs">21 Mins</p>
+											</div>
+											<h1 className="my-1 text-sm mb-2">
+												Palekar Tea Time Rusk | Trans Fat free(300g)
+											</h1>
+
+											<p className="text-xs flex whitespace-nowrap gap-2 text-zinc-500 pb-2 justify-between">
+												<span className="flex">
+													{Array.from({ length: 5 }).map((_, i) => (
+														<Star key={i} size={16} />
+													))}
+													(0)
+												</span>
+												<span>Kilimanjaro iwofe</span>
+											</p>
+
+											<div className="flex justify-between">
+												<div>
+													<p className="text-sm">$29.0</p>
+													<p className="text-xs text-zinc-500">$32.0</p>
+												</div>
+												<div className="bg-blue-500 p-2 text-white rounded-full">
+													<ShoppingCart size={18} />
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</SwiperSlide>
+						))}
+					</Swiper>
 				</div>
 			</section>
 		</main>
