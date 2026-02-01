025261ba5ae497a1b39518f5998bed3de42617ea
thenoobchad <noobchaddy@gmail.com>
2026-02-01 15:32:03 +0100

    fix:login function

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const { signIn, signUp, signOut, useSession } =  createAuthClient({
    
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_URL
 })
